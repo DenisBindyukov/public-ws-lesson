63663afc6fb39684b869ae909c7f7385f70416e8
Denis <gitgroom@gmail.com>
2023-04-02 20:38:01 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import io, {Socket} from "socket.io-client";
+
+const socketIoApi = {
+    socket: null as null | Socket,
+
+    createConnection() {
+        const options = {
+            extraHeaders: {
+                participantId: '1',
+                roomKey: 'test_room'
+            }
+        }
+
+        this.socket = io('http://localhost:5001/participant', options);
+
+        this.socket.on("connect", () => {
+            console.log("ws:connect")
+        })
+
+        this.socket.on("disconnect", (e) => {
+            console.log("ws:disconnect")
+        })
+    },
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [status, setStatus] = useState('');
+    const [quizIsActive, setQuizIsActive] = useState(false);
+
+    const connectSocket = () => {
+        socketIoApi.createConnection()
+        socketIoApi.socket?.on('auth', (data: { status: string }
+        ) => {
+            setStatus(data.status)
+        })
+        socketIoApi.socket?.on('quiz-public', (data: any
+        ) => {
+            console.log('data: ', data)
+            setQuizIsActive(data.payload.status)
+        })
+
+        socketIoApi.socket?.onAny( (data: any
+        ) => {
+            console.log(data)
+        })
+    }
+
+    useEffect(() => {
+        connectSocket()
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Client-Participant</h1>
+                <h3>Participant status: {status ? 'approved' : 'notApproved'}</h3>
+                <h3>Quiz launched: {quizIsActive ? 'TRUE' : 'FALSE'}</h3>
+            </header>
+        </div>
+    );
+
 }
 
 export default App;
